bf12966879d6965fb4b9debacc0d96f0c3a8bf14
miguelvalencia <miguelvalencia2497@gmail.com>
2018-07-19 20:11:56 +0800

    Commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'about_us', to: 'home#about_us', as: :about_us
   get 'contact_us', to: 'home#contact_us', as: :contact_us
+  get 'product_list', to: 'home#product_list', as: :product_list
   post 'contact_email', to: 'home#contact_email'
 
   root 'home#index'
