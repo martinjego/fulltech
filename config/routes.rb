5b0a26526da699f9c289db882780bbf32234bead
martinjego <martsalberto@gmail.com>
2018-09-22 18:02:45 +0800

    add header page for each header

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,14 @@ Rails.application.routes.draw do
   get 'product', to: 'home#product', as: :product
   post 'contact_email', to: 'home#contact_email'
 
+  # Headers
+  get 'comfortable_and_environmental', to: 'home#comfortable_and_environmental', as: :comfortable_and_environmental
+  get 'product_advantages', to: 'home#product_advantages', as: :product_advantages
+  get 'hanma_engine', to: 'home#hanma_engine', as: :hanma_engine
+  get 'go_global', to: 'home#go_global', as: :go_global
+  get 'product_opacity', to: 'home#product_opacity', as: :product_opacity
+  get 'play_an_important_role', to: 'home#play_an_important_role', as: :play_an_important_role
+
   root 'home#index'
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
